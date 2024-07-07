0546509ecb2ce6500b9e69a7012046b0810cd752
IgnacioPuigMargalef <nachopm21@gmail.com>
2024-07-07 18:03:49 +0200

    Correcciones menores

--- gateway-service/src/test/java/com/footballscore/gatewayservice/GatewayServiceApplicationTests.java
+++ gateway-service/src/test/java/com/footballscore/gatewayservice/GatewayServiceApplicationTests.java
@@ -1,13 +1,8 @@
 package com.footballscore.gatewayservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GatewayServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
